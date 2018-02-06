ed4c9e20f03f96294e5cbc237cc40a2a1ca62745
SemenShtanoprud <shtanoprudfamily@gmail.com>
2018-02-06 10:53:53 +0200

    install owl&bootstrap

--- AnyforSoft/src/vendor_entries/vendor.js
+++ AnyforSoft/src/vendor_entries/vendor.js
@@ -1,3 +1,6 @@
 module.exports = [
-  './bower/jquery/dist/jquery.js'
+  './bower/jquery/dist/jquery.js',
+  './bower/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+  './bower/owl.carousel/src/js/owl.carousel.js',
+  './bower/owl.carousel/src/js/owl.navigation.js'
 ];
